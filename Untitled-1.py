ad7c910258abfff905932288a1eaae7bf666d4b0
Dawit Nigus <101523835+Dawit-Nigus@users.noreply.github.com>
2022-05-01 15:13:36 +0300

    Turtle Graphics

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+from turtle import *
+bgcolor('black')
+speed(0)
+hideturtle()
+for i in range(120):
+    color('red')
+    circle(i)
+    color('orange')
+    circle(i*0.8)
+    right(3)
+    forward(3)
+done()
---
+++ Untitled-2.py
@@ -0,0 +1,11 @@
+from turtle import *
+speed(10)
+hideturtle()
+color('cyan')
+bgcolor('black')
+b = 200
+while b > 0:
+    left(b)
+    forward(b * 3)
+    b = b - 1
+    
